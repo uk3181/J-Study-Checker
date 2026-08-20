ef9509e4be90aa9e4f4e7cbcb3b678ddc79de6d6
uk3181 <uk3181@daum.net>
2026-08-20 12:38:39 +0900

    Create app.py

---
+++ SourceCodes/app.py
@@ -0,0 +1,24 @@
+# 최종적으로 실행될 코드
+
+from tkinter import *
+from os import system
+from login_frame import LoginFrame
+
+path: str = '.' # 필요에 따라 변경 가능
+
+class App:
+    def __init__(self) -> None:
+        window: Tk = Tk()
+        window.iconphoto(True, PhotoImage(file = '{}/Images/Icons/app_icon.png'.format(path))) # 윈도우 아이콘 설정
+        window.title('J Study Checker') # 제목 설정
+        window.geometry('600x600') # 창 크기 설정
+        window.resizable(False, False) # 창 크기를 조정하지 못하도록 함.
+
+        loginFrame: LoginFrame = LoginFrame(window)
+        loginFrame.place(x = 0, y = 0)
+
+        window.mainloop()
+
+def main() -> None:
+    App()
+main()
