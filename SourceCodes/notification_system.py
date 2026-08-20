6b17e9c6ad5b83f2fa8d484db5c77e0862cdd24c
uk3181 <uk3181@daum.net>
2026-08-20 12:42:02 +0900

    Create notification_system.py

---
+++ SourceCodes/notification_system.py
@@ -0,0 +1,40 @@
+# 알림 시스템
+
+from user import *
+
+class NotificationSystem:
+    @staticmethod
+    def updateReminderNotification(user: User) -> None: # 리마인더 알림을 업데이트하는 메소드
+        now: dt.datetime = dt.datetime.now()
+        reminderList: SortedList[ReminderData] = user.getReminderList()
+
+        startIndex: int = 0; endIndex: int = len(reminderList) - 1
+        while startIndex <= endIndex:
+            middleIndex: int = (startIndex + endIndex) // 2
+            reminderData: ReminderData = reminderList[middleIndex]
+            if reminderData.getDate().date() > now.date():
+                endIndex = middleIndex - 1
+            elif reminderData.getDate().date() == now.date():
+                notificationList: list[Notification] = user.getNotificationList()
+                for i in range(len(notificationList) - 1, -1, -1):
+                    if notificationList[i].getDate().date() == now.date()\
+                            and notificationList[i].getMemo() == '예약된 리마인더가 있습니다.':
+                        return
+                notificationList.append(Notification(now, '예약된 리마인더가 있습니다.'))
+                user.setNotificationList(notificationList)
+
+                removeUser(user.getId())
+                addUser(user)
+                break
+            else:
+                startIndex = middleIndex + 1
+
+    @staticmethod
+    def updateUserInfoNotification(user: User, infoType: str) -> None: # 사용자 정보 변경 알림을 업데이트하는 메소드
+        notificationList: list[Notification] = user.getNotificationList()
+        notificationList.append(Notification(dt.datetime.now(), '사용자 정보({})가 변경되었습니다.'.format(infoType)))
+
+        removeUser(user.getId())
+        addUser(user)
+
+    # 선형회귀 방법을 통해 일주일, 한 달 간 학습 시간이 증가했는지 확인하여 알림 업데이트하기
